f8f28a94e24592fa8cef99e9522eec4cf0fa9fcc
梁志远 <865522455@qq.com>
2018-08-26 16:38:26 +0800

    添加GYDataCenter库

--- TTNewsSDK/Pods/Target Support Files/Pods-NewsDemo/Pods-NewsDemo-frameworks.sh
+++ TTNewsSDK/Pods/Target Support Files/Pods-NewsDemo/Pods-NewsDemo-frameworks.sh
@@ -112,11 +112,15 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GYDataCenter/GYDataCenter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYImage/YYImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/GYDataCenter/GYDataCenter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYImage/YYImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/YYModel/YYModel.framework"
 fi
